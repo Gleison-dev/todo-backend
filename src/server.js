ff2458d3613d078face99a2d6868afe516441041
Gleison-dev <gleisonrgomes@gmail.com>
2024-10-10 11:30:55 -0300

    fix: importing index route on server

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { testConnection } from "./database/connection.js";
+import { router } from "./routes/index.route.js";
 import dotenv from "dotenv";
 import cors from "cors";
 
@@ -10,6 +11,7 @@ const port = process.env.SV_PORT;
 
 app.use(cors());
 app.use(express.json());
+app.use(router);
 
 app.listen(port, () => {
   testConnection();
