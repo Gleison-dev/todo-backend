d7aebb19e8f2323721b450b0d91ee2be5d4e9b15
Gleison-dev <gleisonrgomes@gmail.com>
2024-10-10 10:20:55 -0300

    feat: task services created

---
+++ src/services/Task.entity.js
@@ -0,0 +1,85 @@
+import { TaskEntity } from "../entities/task.entity.js";
+import { SUCESS } from "../shared/task.message.js";
+
+class TaskService {
+  async createTaskService(task) {
+    try {
+      await TaskEntity.sync();
+      const createTask = await TaskEntity.create({
+        task,
+      });
+      return `${SUCESS.CREATED}`;
+    } catch (error) {
+      return error;
+    }
+  }
+
+  async getAllTaskService() {
+    try {
+      const allTask = await TaskEntity.findAll();
+      return allTask;
+    } catch (error) {
+      return error;
+    }
+  }
+
+  async updateTaskService(id, newTask) {
+    try {
+      const taskId = await TaskEntity.findByPk(id);
+      if (!taskId) {
+        return `${ERRORS.NOT_FOUND}`;
+      }
+      const updateTask = await TaskEntity.update(
+        { task: newTask },
+        {
+          where: {
+            id,
+          },
+        }
+      );
+      return `${SUCESS.UPDATED}`;
+    } catch (error) {
+      return error;
+    }
+  }
+
+  async updateCheckService(id) {
+    try {
+      const taskId = await TaskEntity.findByPk(id);
+      if (!taskId) {
+        return `${ERRORS.NOT_FOUND}`;
+      }
+      const currentCheck = taskId.check;
+      const updateCheck = await TaskEntity.update(
+        { check: !currentCheck },
+        {
+          where: {
+            id,
+          },
+        }
+      );
+      return `${SUCESS.UPDATED}`;
+    } catch (error) {
+      return error;
+    }
+  }
+
+  async deleteTaskService(id) {
+    try {
+      const taskId = await TaskEntity.findByPk(id);
+      if (!taskId) {
+        return `${ERRORS.NOT_FOUND}`;
+      }
+      const deleteTask = await TaskEntity.destroy({
+        where: {
+          id,
+        },
+      });
+      return `${SUCESS.DELETED}`;
+    } catch (error) {
+      return error;
+    }
+  }
+}
+
+export { TaskService };
