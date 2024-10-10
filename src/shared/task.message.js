3d27fbb48a3fcd3900a8e6b30e520ce6d377cd4f
Gleison-dev <gleisonrgomes@gmail.com>
2024-10-10 10:22:07 -0300

    feat: addind message error

--- src/shared/task.message.js
+++ src/shared/task.message.js
@@ -1,7 +1,11 @@
+const ERRORS = {
+    NOT_FOUND: "Tarefa não encontrada. Tente novamente!"
+}
+
 const SUCESS = {
     CREATED: "Tarefa criada com sucesso!",
     UPDATED: "Tarefa alterada com sucesso!",
     DELETED: "Tarefa deletada com sucesso!"
 }
 
-export { SUCESS }
+export { ERRORS, SUCESS }
